616bc959b1a590027589053de06062cb0d75d04d
Flafy <flafyarazi@gmail.com>
2023-02-09 15:13:26 +0200

    fix binary tree functions

--- Lessions/Les22/Les22.cs
+++ Lessions/Les22/Les22.cs
@@ -6,60 +6,48 @@ namespace Les22
 {
     class Les22 : Lession
     {
-        public static int CountAllBNodes(BinNode<int> bt)
-        {
-            if (bt == null) return 0;
-            return CountAllBNodes(bt.GetLeft()) + CountAllBNodes(bt.GetRight()) + 1;
-        }
+     public static int CountAllBNodes(BinNode<int> bt)
+    {
+      if (bt == null) return 0;
+      return CountAllBNodes(bt.GetLeft()) + CountAllBNodes(bt.GetRight()) + 1;
+    }
 
-        public static int SumTree(BinNode<int> bt)
-        {
-            if (bt == null) return 0;
-            return SumTree(bt.GetLeft()) + SumTree(bt.GetRight()) + bt.GetValue();
-        }
+    public static int SumTree(BinNode<int> bt)
+    {
+      if (bt == null) return 0;
+      return SumTree(bt.GetLeft()) + SumTree(bt.GetRight()) + bt.GetValue();
+    }
 
-        public static int SumOfEvenBNodes(BinNode<int> bt)
-        {
-            if (bt == null) return 0;
-            int val = bt.GetValue();
+    public static int SumOfEvenBNodes(BinNode<int> bt)
+    {
+      if (bt == null) return 0;
+      int val = 0;
 
-            if (val % 2 == 1) val = 0;
+      if (val % 2 == 0) val = bt.GetValue();
 
-            return SumTree(bt.GetLeft()) + SumTree(bt.GetRight()) + bt.GetValue();
-        }
+      return SumOfEvenBNodes(bt.GetLeft()) + SumOfEvenBNodes(bt.GetRight()) + val;
+    }
 
-        public static int SumOfLeavesBNodes(BinNode<int> bt)
-        {
-            if (bt == null) return 0;
-            if (bt.GetLeft() == null && bt.GetRight() == null) return bt.GetValue();
-            return SumTree(bt.GetLeft()) + SumTree(bt.GetRight());
-        }
+    public static int SumOfLeavesBNodes(BinNode<int> bt)
+    {
+      if (bt == null) return 0;
+      if (bt.GetLeft() == null && bt.GetRight() == null) return bt.GetValue();
+      return SumOfLeavesBNodes(bt.GetLeft()) + SumOfLeavesBNodes(bt.GetRight());
+    }
 
-        public static int CountLeavesBNodes(BinNode<int> bt)
-        {
-            if (bt == null) return 0;
-            if (bt.GetLeft() == null && bt.GetRight() == null) return 1;
-            return SumTree(bt.GetLeft()) + SumTree(bt.GetRight());
-        }
+    public static int CountLeavesBNodes(BinNode<int> bt)
+    {
+      if (bt == null) return 0;
+      if (bt.GetLeft() == null && bt.GetRight() == null) return 1;
+      return CountLeavesBNodes(bt.GetLeft()) + CountLeavesBNodes(bt.GetRight());
+    }
 
-        public static int CountOnlyEvenBNodes(BinNode<int> bt)
-        {
-            if (bt == null) return 0;
-            int val = 1;
+    public static int CountOnlyEvenBNodes(BinNode<int> bt)
+    {
+      if (bt == null) return 0;
 
-            if (val % 2 == 1) val = 0;
-
-            return SumTree(bt.GetLeft()) + SumTree(bt.GetRight()) + bt.GetValue();
-        }
-
-
-        public static void PrintTree(BinNode<int> bt)
-        {
-            if (bt == null) return;
-            PrintTree(bt.GetLeft());
-            Console.Write(bt.GetValue() + " ");
-            PrintTree(bt.GetRight());
-        }
+      return CountOnlyEvenBNodes(bt.GetLeft()) + CountOnlyEvenBNodes(bt.GetRight()) + (1 - bt.GetValue() % 2);
+    }
 
         public override void Main()
         {
